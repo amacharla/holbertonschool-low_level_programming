9d17ae1108dd5352637a8aa46b2d5e93c5161ac7
amacharla <noop2016@gmail.com>
2017-05-11 22:31:51 +0000

    5 logic understood

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * init char and using while loop to go through the alphabet
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
