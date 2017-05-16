0ce5f9d14f8537a41717f0ff232a852a896e0e2f
amacharla <noop2016@gmail.com>
2017-05-16 22:34:48 +0000

    8 done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,29 @@
 #include "holberton.h"
-
+/**
+ * jack_bauer - its a countdown
+ * Learned to user the limits of intergers to my advantage
+ * Used '/' to get the first digit
+ * used '%' to get the last digit
+ */
 void jack_bauer(void)
 {
+	int inner = 00;
+	int outter = 00;
+
+	while (outter <= 23)
+	{
+		while (inner <= 59)
+		{
+			_putchar((outter / 10) + '0');
+			_putchar((outter % 10) + '0');
+			_putchar(':');
+			_putchar((inner / 10) + '0');
+			_putchar((inner % 10) + '0');
+			_putchar('\n');
+			inner++;
+		}
+		inner = 00;
+		outter++;
+	}
 
 }
