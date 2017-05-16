716aeeeffb3d70bc89eb910a430e08278f0b0468
amacharla <noop2016@gmail.com>
2017-05-16 20:38:36 +0000

    made the count print 10 lines intead of 11

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@ void print_alphabet_x10(void)
 	char az = 'a';
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		while (az <= 'z')
 		{
 			_putchar(az);
 			az++;
 		}
-		i++;
+		++i;
 		az = 'a';
 		_putchar('\n');
 	}
