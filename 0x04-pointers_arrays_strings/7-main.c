2028069a00a33a31735981ada792058c4cf7b827
amacharla <noop2016@gmail.com>
2017-05-24 01:47:19 +0000

    7 done

--- 0x04-pointers_arrays_strings/7-main.c
+++ 0x04-pointers_arrays_strings/7-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    str = "0123456789";
+    str = "012345789";
     puts_half(str);
     return (0);
 }
---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * puts_half - prints second half of string
+ * @str: takes in array type char
+ */
+void puts_half(char *str)
+{
+	int i, j;
+	int size, half;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+	size = (i - 1);
+	if (size % 2 == 0)
+		half = size / 2;
+	else
+		half = (size + 1) / 2;
+	for (j = half; j <= size; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
