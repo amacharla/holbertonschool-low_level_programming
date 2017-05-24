d93d6667c13e68090ff442da19a4bf4d0f642492
amacharla <noop2016@gmail.com>
2017-05-24 05:42:42 +0000

    100 attempt

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+int _atoi(char *s)
+{
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (('0' >= s[i]) && ('9' <= s[i]))
+		{
+			if (s[i-1] == '-')
+				sign = s[i-1];
+			while a
+	}
+}
