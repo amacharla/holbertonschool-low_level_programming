e40e8a7771c12c4f2804813d47b4e4de27877f89
amacharla <noop2016@gmail.com>
2017-05-24 19:34:48 +0000

    adsf

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,18 @@
 #include "holberton.h"
+#include <stdio.h>
 
 int _atoi(char *s)
 {
+	int i, num;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == 45)
+			num = s[i-1];
 		if (('0' >= s[i]) && ('9' <= s[i]))
-		{
-			if (s[i-1] == '-')
-				sign = s[i-1];
-			while a
+		{	putchar(s[i]);	
+			num = (num * 10) + s[i];
+		}
 	}
+	return (0);
 }
