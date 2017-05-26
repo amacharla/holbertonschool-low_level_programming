aeff5999e3808267d85654969b4c844dee1adc93
amacharla <noop2016@gmail.com>
2017-05-26 01:03:24 +0000

    5 done

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * string_toupper - converts lowercase to uppercase
+ * @s: pointer of type char array
+ * Return: s which is uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
