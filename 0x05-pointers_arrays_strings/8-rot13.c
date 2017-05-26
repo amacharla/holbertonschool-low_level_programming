0c4c36660d2491253d001ae4a0d9b3993245c3f0
amacharla <noop2016@gmail.com>
2017-05-26 05:29:17 +0000

    8 done

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -1,13 +1,20 @@
-
-
+/**
+ * rot13 - encode in rot13
+ * @s: pointer to char array
+ * Return: char array s.
+ */
 char *rot13(char *s)
 {
 	int i;
 
 	for (i = 0; s[i]; i++)
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z') && (s[i] >= 'A' && s[i] <= 'Z'))
-				s[i] = s[i] + 13;
+		if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
+			s[i] = s[i] + 13;
+		else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			s[i] = s[i] - 13;
+		else
+			s[i] = s[i];
 	}
 	return (s);
 }
