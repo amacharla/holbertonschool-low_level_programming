873c7608312bdb3ad66d89af155b607e1ad44738
amacharla <noop2016@gmail.com>
2017-05-26 05:52:02 +0000

    6 redone

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@ char *cap_string(char *s)
 {
 	int i;
 
-	if ((s[0] >= 'a' && s[0] <= 'z')
+	if ((s[0] >= 'a' && s[0] <= 'z') && (s[0] >= 'A' && s[0] <= 'z'))
 		s[0] = s[0] - 32;
 	for (i = 0; s[i]; i++)
 	{
@@ -16,7 +16,7 @@ char *cap_string(char *s)
 			case ' ': case ',': case ';': case '.': case '!': case '?': case '"':
 			case '(': case ')': case '{': case '}': case '\t': case '\n':
 
-				if (s[i] >= 'a' && s[i] <= 'z')
+				if ((s[i] >= 'a' && s[i] <= 'z') && (s[i] >= 'A' && s[i] <= 'z'))
 					s[i] = s[i] - 32;
 		}
 	}
