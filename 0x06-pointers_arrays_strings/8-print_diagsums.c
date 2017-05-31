18f0279484bb0ebc26fb53c11749734b3471853d
amacharla <noop2016@gmail.com>
2017-05-31 22:26:55 +0000

    5 and 7 fixed

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-
+/**
+ * print_diagsums -  prints sum of 2 diagonals of square matrix of integers
+ * @a: 2dimiational array being casted into pointer
+ * @size: size of square matrix
+ * ld - left digonal sum
+ * rd - right diagonal sum
+ */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int sum = 0;
-	for (i = 0; i <= size; i++)
+	int rd = 0;
+	int ld = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= size; j++)
-		{
-			if (i == j)
-			{
-				printf("i = %d and J = %d\n", i, j);
-				printf("a[i][j] = %d\n", *(a + i * size));
-				sum = a[i + j] + sum;
-			}
-		}
+		ld += *(a + (i * size) + i);
+		rd += *(a + (i * size) + size - 1 - i);
 	}
-	printf("%d, ", sum);
+	printf("%d, %d\n", ld, rd);
 }
