70a95172051531623540bdc993d30bdefa1eefb5
amacharla <noop2016@gmail.com>
2017-06-02 17:19:34 +0000

    6 fixed

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -5,11 +5,9 @@
  */
 int is_prime_number(int n)
 {
-	if (n < 0)
-		n *= -1;
 	if (n == 2 || n == 3 || n == 5 || n == 7)
 		return (1);
-	if (n == 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+	if (n <= 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
 		return (0);
 	return (1);
 }
