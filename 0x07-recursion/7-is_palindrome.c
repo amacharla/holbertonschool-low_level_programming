00da4fb9bb3d72ffb009553cd8badd058abd94b3
amacharla <noop2016@gmail.com>
2017-06-02 07:09:20 +0000

    7 betty

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
-
+/**
+ * palindromeCheck - checks if 1 is equal to last
+ * @s: is a pointer to char string
+ * @length: length of last string
+ * Return: 5 if equal and 0 if not
+ */
 int palindromeCheck(char *s, int length)
 {
 	if (*s == '\0')
 		return (5);
 	if (s[0] == s[length])
 		return (palindromeCheck(s + 1, length));
-	
+
 	printf("*s is %c |", *s);
 	putchar('\t');
 	printf("| s[%d] is %c\n", length, *(s + length));
 
 	return (0);
 }
-
+/**
+ * is_palindrome - counts the length
+ * @s: pointer to string
+ * Return: 1 if plandrom and 0 if isnt
+ */
 int is_palindrome(char *s)
 {
-	int length; 
-	
-	if (*s + 1 = '\0')
+	int length;
+
+	if (*s + 1 == '\0')
 		return;
 	if (*s == '\0')
 		return (0);
-	
+
 	length = ((is_palindrome(s + 1) + 1) - 1);
 	/*return (palindromeCheck(s, length));*/
 	return (length);
