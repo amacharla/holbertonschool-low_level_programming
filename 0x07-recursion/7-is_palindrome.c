66b9db8915149c2914467d39a1860e1bc31ec562
amacharla <noop2016@gmail.com>
2017-06-02 19:25:43 +0000

    7 bug fixed with Falicas help

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
 /**
  * palindromeCheck - checks if 1 is equal to last
- * @s: is a pointer to char string
- * @length: length of last string
- * Return: 5 if equal and 0 if not
+ * @s: char array pointing to the begenning of string
+ * @e: char array pointing to the end of the string
+ * Return: 1 if equal and 0 if not
  */
-int palindromeCheck(char *s, int length)
+int palindromeCheck(char *s, char *e)
 {
 	if (*s == '\0')
-		return (5);
-	if (s[0] == s[length])
-		return (palindromeCheck(s + 1, length));
-
-	printf("*s is %c |", *s);
-	putchar('\t');
-	printf("| s[%d] is %c\n", length, *(s + length));
-
+		return (1);
+	if (s[0] == e[0])
+		return (palindromeCheck(s + 1, e - 1));
 	return (0);
+}
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ * it goes all the way to null then returns 0 then adds +1
+ * down till it reaches the begenning again.
+ * its counting from the end to front
+ * Return: int of length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
 /**
  * is_palindrome - counts the length
@@ -26,13 +34,12 @@ int palindromeCheck(char *s, int length)
 int is_palindrome(char *s)
 {
 	int length;
+	char *e;
 
-	if (*s + 1 == '\0')
-		return;
 	if (*s == '\0')
-		return (0);
+		return (1);
 
-	length = ((is_palindrome(s + 1) + 1) - 1);
-	/*return (palindromeCheck(s, length));*/
-	return (length);
+	length = (_strlen_recursion(s));
+	e = (s + length) - 1;
+	return (palindromeCheck(s, e));
 }
