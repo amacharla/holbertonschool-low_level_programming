6ed2f77b9d11ab43fc3bd02f0b073fb91f61f0dc
amacharla <noop2016@gmail.com>
2017-06-04 23:57:31 +0000

    0 fixed betty

--- 0x08-static_libraries/main.c
+++ 0x08-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
-
+/**
+ * main - testeing static library
+ * Return: 0 success
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker");
+	_puts("\"\n\t- Kevin Mitnick");
+	return (0);
 }
