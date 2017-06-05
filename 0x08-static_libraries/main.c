fb110345c6e62a3f36d43e06b81f81ca7880c2b1
amacharla <noop2016@gmail.com>
2017-06-05 00:01:05 +0000

    0: fixed newline issue

--- 0x08-static_libraries/main.c
+++ 0x08-static_libraries/main.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker");
-	_puts("\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"");
+	_puts("\t- Kevin Mitnick");
 	return (0);
 }
