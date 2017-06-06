f9d961451f041d0cc100beb7a2589a06e0a37113
amacharla <noop2016@gmail.com>
2017-06-06 21:59:12 +0000

    0: changed 0 to null

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -10,11 +10,11 @@ char *create_array(unsigned int size, char c)
 	char *a = (char *) malloc(size * sizeof(char));
 	unsigned int i;
 
-	if (size == 0)
-		return (0);
+	if (size == NULL)
+		return (NULL);
 
-	if (a == 0)
-		return (0);
+	if (a == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		a[i] = c;
