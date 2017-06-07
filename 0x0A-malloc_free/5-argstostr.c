004181dae07807f4f7509da3cfed0bfaf68062f0
amacharla <noop2016@gmail.com>
2017-06-07 23:54:32 +0000

    5: done with Topes help

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,24 +1,42 @@
 #include <stdlib.h>
-
+/**
+ * argstostr - concatenates all the arguments of my program
+ * @ac: argument count
+ * @av: argument array
+ * Return: pointer to string
+ */
 char *argstostr(int ac, char **av)
 {
-	int i, j;
-	char **b;
+	int i, j, k = 0;
+	char *b;
+	int size = 0;
 
-	b = malloc(ac * sizeof(char));
-	for (i = 1; i < ac; i++)
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i]; a++)
-			b[i] = malloc(
+		for (j = 0; av[i][j]; j++)
+			size++;
+		size++;
 	}
+	size++;
 
-	for (i = 1; i < ac; i++)
+	b = malloc(size * sizeof(char));
+	if (b == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[j]; j++)
-			b[j] = av[j];
-		b[j] = '\n';
+		for (j = 0; av[i][j]; j++)
+		{
+			b[k] = av[i][j];
+			k++;
+		}
+		b[k] = '\n';
+		k++;
 	}
-	
+	b[k] = '\0';
+
 	return (b);
 
 }
