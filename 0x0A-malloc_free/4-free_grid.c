13fb190514c3d72d9fa3efd1be7c05adba0d6482
amacharla <noop2016@gmail.com>
2017-06-07 04:49:01 +0000

    4: done

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,46 +1,16 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- * alloc_grid - initilizes and return pointer to a 2 dimensional array of
- * integers.
- * @width: pointers to hold pointer
- * @height: pointer being filled with 0
- * Return: Null if no memory is allocated or double pointer if space created
- * or null
- * height or width is negative or 0
+ * free_grid - frees up frees a 2 dimensional grid previously created by your alloc_grid function.
+ * @grid: pointers to hold pointer
+ * @height: height of the grid
  */
-int **alloc_grid(int width, int height)
-{	/*width = collumns | height = rows*/
 
-	int **a; /*pointer to hold pointers*/
-	int i, j;
+void free_grid(int **grid, int height)
+{
+	int i;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-	/*save pointer space * width amount of size*/
-	a = malloc(width * sizeof(int *));
-	if (a == NULL)
-	{
-		free(a);
-		return (NULL);
-	}
-/*fill each pointer space with space in form of 0 height amount of times*/
-	for (i = 0; i < width; i++)
-	{
-		a[i] = malloc(height * sizeof(int));
-		if (a[i] == NULL)
-		{
-			for (j = 0; j <= i; j++)
-				free(a[j]);
-			free(a);
-			return (NULL);
-		}
-	}
-	for (i = 0; i < width; i++)
-	{
-		for (j = 0; j < height; j++)
-			a[i][j] = 0;
-	}
-	return (a);
-
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
