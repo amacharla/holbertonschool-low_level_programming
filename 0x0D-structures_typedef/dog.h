d505433728720158cec2cd9036564362bc46153a
amacharla <noop2016@gmail.com>
2017-06-12 23:10:50 +0000

    3 done with betty comments

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -15,5 +15,9 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 #endif /*_0_DOG_H_*/
