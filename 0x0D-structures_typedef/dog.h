bd91229c9bdd2fd40dffb4b7905f4fdd63ab60dd
amacharla <noop2016@gmail.com>
2017-06-12 22:15:39 +0000

    fixed betty for dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog - contains needed info about dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dogs owner
+ */
 struct dog
 {
 	char *name;
@@ -10,8 +15,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/*dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);*/
 
 #endif /*_0_DOG_H_*/
