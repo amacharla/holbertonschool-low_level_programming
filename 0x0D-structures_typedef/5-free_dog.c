24fbbf48281b38356e341d95836106a152b2c108
amacharla <noop2016@gmail.com>
2017-06-13 01:12:40 +0000

    5: done

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -3,8 +3,11 @@
 /**
  * free_dog - free allocated memory
  * @d: pointer to my_dog which has the address of to my newdog pointer
+ * free for name and owner then whole structure
  */
 void free_dog(dog_t *d)
 {
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
