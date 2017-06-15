a6da24c3a8c5a920f46659f6b8038b5ac19acbb4
amacharla <noop2016@gmail.com>
2017-06-15 03:56:13 +0000

    3 fixed messages and took care of multiple char for operator

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -6,6 +6,7 @@
  */
 int (*get_op_func(char *s))(int num1, int num2)
 {
+	/*array of structures of struct op*/
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -13,14 +14,15 @@ int (*get_op_func(char *s))(int num1, int num2)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	};
+	};	/*op , function address that takes in (int, int) returns int*/
 	int i = 0;
 
+	/*while the elements of the op isnt null*/
 	while (ops[i].op)
-	{
-		if (ops[i].op[0] == *s)
-			return (ops[i].f);
-		i++;
+	{	/*operator = sign and sign is the only character*/
+		if (ops[i].op[0] == *s && s[1] == '\0')
+			return (ops[i].f); /*return pointer to function to respective operator*/
+		i++; /*looks through all the operators in array ops.op*/
 	}
 	return (NULL);
 }
--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -8,31 +8,33 @@
 int main(int argc, char **argv)
 {
 	int num1, num2, result;
-	int (*func)(int, int);
-
+	int (*func)(int, int);/*hold pointer to function with 2 int args*/
+	/*must have 3 arguments*/
 	if (argc < 4)
 	{
-		puts("Error argc");
+		puts("Error");
 		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
+	/*prevents undefined computation*/
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
-		puts("Error denomator is 0");
+		puts("Error");
 		exit(99);
 	}
-
+	/*sends symbol of operator > gets pointer to right function for computation*/
 	func = get_op_func(argv[2]);
-	result = func(num1, num2);
-
-	if (!(func))
+	/*if func returns null then symbol doesn match arithmetic operator*/
+	if (func == NULL)
 	{
-		puts("Error invald operator");
+		puts("Error");
 		exit(100);
 	}
+	/*func replactes respectice calc based of symbol given to func from line 29*/
+	result = func(num1, num2);
 
 	printf("%d\n", result);
 	return (0);
