45fca3806759edea168f6914f38be7c7cded9b23
amacharla <noop2016@gmail.com>
2017-06-27 21:11:01 +0000

    7 added an && statement instead

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && head; i++)
 		head = head->next;
 
 	if (head == NULL)
