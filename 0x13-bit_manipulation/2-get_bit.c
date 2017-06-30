0b765aa5f6cd2bf24ca0008c21e871a9a4fef58b
amacharla <noop2016@gmail.com>
2017-06-30 23:22:08 +0000

    1 redone

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,1 +1,13 @@
 #include "holberton.h"
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i, result = 0;
+
+	for (i = index; i > 0; i--)
+	{
+		result *= 2;
+		result 
+
+	}
+}
