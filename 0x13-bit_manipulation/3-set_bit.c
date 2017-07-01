8167545818d32150cddabd59621c4123f679a09e
Anoop M <noop2016@gmail.com>
2017-06-30 20:58:43 -0700

    done in car

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,1 +1,14 @@
 #include "holberton.h"
+/**
+ * set_bit -sets the value of a bit to 1 at a given index.
+ * @n: pointer to long
+ * @index: bit thats being shifted
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+  if (index > 63)
+      return (-1);
+  *n = *n | (1L << index);
+  return (1);
+}
