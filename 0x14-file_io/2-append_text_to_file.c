197d028ad9509c61fabef3aafc59ce2306664d21
amacharla <noop2016@gmail.com>
2017-07-05 01:16:34 +0000

    2: if text_content is null

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -26,10 +26,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1 || text_content == NULL)
+	if (file == -1)
 		return (-1);
-
-	check = write(file, text_content, _strlen(text_content));
+	if (text_content)
+		check = write(file, text_content, _strlen(text_content));
 	if (check == -1)
 	{
 		close(file);
