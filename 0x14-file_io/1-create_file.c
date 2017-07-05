139a0e8590a18dd555009146323946b17c7839cb
amacharla <noop2016@gmail.com>
2017-07-05 01:14:17 +0000

    2: done

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -25,15 +25,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	/*if text_content is null create empty file*/
 	if (text_content == NULL)
 	{
 		text_content = "";
 	}
 
+	/*create or Truncate and set privilates to rw for owner*/
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 		return (-1);
 
+	/*write to file, the content , content length*/
 	check = write(file, text_content, _strlen(text_content));
 	if (check == -1)
 	{
---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * _strlen - gets string length
+ * @text: character pointer to string
+ * Return: size of string
+ */
+size_t _strlen(char *text)
+{
+	size_t i;
+
+	for (i = 0; text[i]; i++)
+		;
+	return (i);
+}
+/**
+ * append_text_to_file - append a text at the end of a file
+ * @filename: name of file to append
+ * @text_content: content thats being append to file
+ * Return: 1 succesful or -1 unsuccessful
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, check;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1 || text_content == NULL)
+		return (-1);
+
+	check = write(file, text_content, _strlen(text_content));
+	if (check == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+	return (1);
+}
