310869fa7f1cbdcb1179c2d7a622c15a7ae9d53d
amacharla <noop2016@gmail.com>
2017-07-06 21:19:43 +0000

    3: fixed close error

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -60,7 +60,7 @@ int cp(char *copy, char *paste)
 	if (check == -1 || check2 == -1)
 	{
 		fd = ((check == -1) ? fdC : fdP);
-		dprintf(STDERR_FILENO, " Error: Can't close fd %d\n", fd), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
 	}
 	return (0);
 }
