960586c9d49d40e288bcd51b2145c2b284e20f82
amacharla <noop2016@gmail.com>
2017-07-06 21:10:01 +0000

    3: fixed turnury operator

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -59,7 +59,7 @@ int cp(char *copy, char *paste)
 	check = close(fdC), check2 = close(fdP);
 	if (check == -1 || check2 == -1)
 	{
-		fd = check == -1 ? fdC : fdP;
+		fd = ((check == -1) ? fdC : fdP);
 		dprintf(STDERR_FILENO, " Error: Can't close fd %d\n", fd), exit(100);
 	}
 	return (0);
