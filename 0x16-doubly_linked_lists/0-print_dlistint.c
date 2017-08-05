049d1b6a96540364757c31bcd8f833b50888a4df
amacharla <noop2016@gmail.com>
2017-08-05 21:34:08 +0000

    0 done

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,17 @@
 #include "lists.h"
+/**
+ * print_dlistint - prints data and returns size of list
+ * @h: pointer to head
+ * Return: size of list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t i;
 
+	for (i = 0; h; i++)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
