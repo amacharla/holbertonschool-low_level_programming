19bc2f908949a3530c0664b6a1e03999a7bbc42e
amacharla <noop2016@gmail.com>
2017-08-05 23:03:37 +0000

    3 fixed

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,20 @@
 #include "lists.h"
-
+/**
+ * add_dnodeint - adds node to the begenning of the list
+ * @head: double pointer to head
+ * @n: data being added to the new node
+ * Return: new node or NULL if malloc fails
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (new == NULL || head == NULL)
+		return (NULL);
+/*Do i not need to link the *head-prev = new?*/
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,32 @@
 #include "lists.h"
-
+/**
+ * add_dnodeint_end - adds node at the end
+ * @head: double pointer to head
+ * @n: data
+ * Return: pointer to new node or NULL
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
+	dlistint_t *new, *current = *head;
 
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;/*make current node last*/
+	if (current == NULL)
+	{
+		*head = new;/*USE THIS INSTEAD OF CURRENT*/
+		new->prev = NULL;
+		return (new);
+	}
+	/* Get to the last node */
+	for (; current->next != NULL; current = current->next)
+		;
+	new->prev = current;/*set last node to currents prev*/
+	current->next = new;/*have old last node next point last node*/
+	return (new);
 }
