92fabeb2a2155b8a48d956677f5063dea243bbef
amacharla <noop2016@gmail.com>
2017-08-06 22:54:58 +0000

    6 done

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,14 @@
 #include "lists.h"
-
+/**
+ * sum_dlistint - gets sum of all n in linked list
+ * @head: pointer to each node
+ * Return: sum of all n or 0
+ */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
 
+	for (; head; head = head->next)
+		sum += head->n;
+	return (sum);
 }
