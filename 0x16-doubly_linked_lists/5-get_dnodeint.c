e7403cfe0e1bba67d490d243afd9ba2d991631be
amacharla <noop2016@gmail.com>
2017-08-06 22:48:35 +0000

    5 done

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,17 @@
 #include "lists.h"
-
+/**
+ * get_dnodeint_at_index - get node at index
+ * @head: pointer to nodes to go through the linked list
+ * @index: position to return the node
+ * Return: NULL or node at index
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i;
 
+	for (i = 0; i < index && head; i++)
+		head = head->next;
+	if (head == NULL)
+		return (NULL);
+	return (head);
 }
