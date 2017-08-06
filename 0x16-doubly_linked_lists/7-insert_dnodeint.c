52b8be66e3f09c1c96175506c9cb516cc4b8a382
amacharla <noop2016@gmail.com>
2017-08-06 23:21:48 +0000

    6 done

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,5 +2,20 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	unsigned int i;
+	dlistint_t *new;
 
+	if (h == NULL)
+		return (NULL);
+	for (i = 0; i < idx && (*h)->next; i++)
+		*h = (*h)->next;
+	if (*h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = *h->prev;
+	i == idx ? new->next = *h : new->next = NULL;
+	return (new);
 }
