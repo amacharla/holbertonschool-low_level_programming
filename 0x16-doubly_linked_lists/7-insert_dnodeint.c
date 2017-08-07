24aa77199b03bd21edd1d739b40b6a6725ccede7
amacharla <noop2016@gmail.com>
2017-08-07 01:33:28 +0000

    7 still working

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,21 +1,46 @@
 #include "lists.h"
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *new_node(int n)
 {
-	unsigned int i;
-	dlistint_t *new;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-	if (h == NULL)
-		return (NULL);
-	for (i = 0; i < idx && (*h)->next; i++)
-		*h = (*h)->next;
-	if (*h == NULL)
-		return (NULL);
-	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->prev = *h->prev;
-	i == idx ? new->next = *h : new->next = NULL;
 	return (new);
+}
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *new, *current = *h;
+
+	if (h == NULL)
+		return (NULL);
+
+	for (; current->next != NULL; current = current->next)/*if in list*/
+	{
+		if (i == idx)
+		{
+			new = new_node(n);
+			if (new == NULL)
+				return (NULL);
+			new->prev = current->prev;
+			new->next = current;
+			current->prev = new;
+			return (new);
+		}
+		i++;
+	}
+	if (current->next == NULL && i == idx)/*IF tail*/
+	{
+		new = new_node(n);
+		if (new == NULL)
+			return (NULL);
+		new->prev = current;
+		new->next = NULL;
+		current->next = new;
+		return (new);
+	}
+	return (NULL);
 }
