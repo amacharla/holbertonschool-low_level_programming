833cc2cac5b38d4891db30af22446dfb6395202b
amacharla <noop2016@gmail.com>
2017-08-12 02:43:13 +0000

    7 done

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * new_node - creates a new node and assigns value
+ * @n: value being assigned to data
+ * Return: pointer to new node
+ */
 dlistint_t *new_node(int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
@@ -9,38 +13,46 @@ dlistint_t *new_node(int n)
 	new->n = n;
 	return (new);
 }
-
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to head
+ * @idx: the index of the list where the new node should be added
+ * @n: data for new node
+ * Return: the address of the new node, or NULL if it failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	dlistint_t *new, *current = *h;
 
-	if (h == NULL)
+	if (h == NULL || (current == NULL && idx != 0))
 		return (NULL);
 
-	for (; current->next != NULL; current = current->next)/*if in list*/
+	new = new_node(n);
+	if (new == NULL)
+		return (NULL);
+	if (idx == 0)/*incerting in the begenning*/
 	{
-		if (i == idx)
-		{
-			new = new_node(n);
-			if (new == NULL)
-				return (NULL);
-			new->prev = current->prev;
-			new->next = current;
+		new->prev = NULL;
+		if (current == NULL) /*empty list*/
+			current = new;
+		else
 			current->prev = new;
-			return (new);
-		}
-		i++;
+		new->next = current;
 	}
-	if (current->next == NULL && i == idx)/*IF tail*/
+	for (i = 0; i < idx - 1; i++)/*transvers and stop before idx*/
 	{
-		new = new_node(n);
-		if (new == NULL)
+		if (current->next == NULL)/*if idx > listsize return*/
+		{
+			free(new);
 			return (NULL);
-		new->prev = current;
-		new->next = NULL;
-		current->next = new;
-		return (new);
+		}
+		current = current->next;
 	}
-	return (NULL);
+	new->prev = current;/*insert at idx*/
+	new->next = current->next; /* current - new - Current->next */
+	current->next = new;
+	if (current->next != NULL)
+		current->next->prev = new;
+	return (new);
 }
--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,5 +2,33 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *next, *current = *head;
+	unsigned int i;
 
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		next = (*head)->next;
+		free(*head);
+		if (next != NULL)
+		{
+			next->prev = NULL;
+			return (1);
+		}
+	}
+	for (i = 0; i < index; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	current->prev->next = current->next;
+	if (current->next)
+	{
+		current->next->prev = current->prev;
+		free(current);
+		return (1);
+	}
+	return (1);
 }
