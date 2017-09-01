d9a0d3d3b2180566f035a9958bcc15a7002d6124
amacharla <noop2016@gmail.com>
2017-09-01 21:47:41 +0000

    4 done

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *bucket;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size); /* get keys index */
+
+	bucket = ht->array[index]; /* go to index of where key is located */
+
+	for (; bucket; bucket = bucket->next) /* look for key in bucket */
+		if (strcmp(bucket->key, key) == 0)
+			return (bucket->value);
+	return (NULL);
+}
