c835f44e9be64c37476b6bb1270e6b8ab55c35eb
Anoop M <noop2016@gmail.com>
2017-09-19 19:19:56 +0000

    3 redo edge case

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -7,7 +7,7 @@
  **/
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 2)
+	if (array == NULL || size < 2)
 		return;
 	/* use coustom function to make recursion easier */
 	quick_sort_recurse(array, size, 0, size - 1);
