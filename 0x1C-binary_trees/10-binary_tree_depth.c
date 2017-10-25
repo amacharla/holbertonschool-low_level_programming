f4162eacd00d011a099feba073217320830a6ef6
Anoop M <noop2016@gmail.com>
2017-10-25 03:51:53 +0000

    10 done

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -1,6 +1,10 @@
-#include "binary_tree.h"
+#include "binary_trees.h"
 
 size_t binary_tree_depth(const binary_tree_t *node)
 {
+	size_t count = 0;
 
+	for (; node; node = node->parent)
+		count++;
+	return count - 1;
 }
