e277bbfcca75e8a2b06a35aac79b902b893a600e
Anoop M <noop2016@gmail.com>
2017-10-25 03:38:33 +0000

    to 9 done

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,6 +1,17 @@
-#include "binary_tree.h"
+#include "binary_trees.h"
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left;
+	size_t right;
+			    /*---------- if leaf ------------- */
+	if (tree == NULL || (tree && !(tree->left || tree->right)))
+		return 0;
+	else
+	{
+		left = binary_tree_height(tree->left);
+		right = binary_tree_height(tree->right);
 
+		return (left < right ? left + 1 : right + 1);
+	}
 }
