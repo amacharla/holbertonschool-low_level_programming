8e49a070eefaac8647b10f14d820fe839407fc3c
Anoop M <noop2016@gmail.com>
2017-12-06 21:34:32 +0000

    added comments

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -10,20 +10,25 @@
 int jump_search(int *array, size_t size, int value)
 {
 	size_t i;
-	int jump = (int) sqrt(size);
+	int jump;
 
-	for (i = 0; i < size; i += jump)
+	if (array == NULL || size == 0) /* argument check */
+		return (-1);
+
+	jump = (int) sqrt(size); /* skip interval */
+
+	for (i = 0; i < size; i += jump) /* start off skipping */
 	{
 		if (array[i] == value)
 			return (i);
 		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
-		if (array[i] > value)
+		if (array[i] > value) /*reset params to preform linear search*/
 		{
-			size = i;
-			i -= jump;
-			jump = 1;
+			size = i;  /* know the value is less than index i */
+			i -= jump; /* jump one level back */
+			jump = 1; /*linear search */
 			printf("Value found between indexes [%lu] and [%lu]\n", i, size);
 		}
 	}
-	return (-1);
+	return (-1); /* no match found */
 }
